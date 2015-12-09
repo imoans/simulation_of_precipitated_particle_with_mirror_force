af6f4a34e5df16ffc537ab34c1039b78acc04b2d
Naomi Shoji <naomins.0101@gmail.com>
2015-12-09 13:34:35 +0900

    磁場を非一様に

--- rungekutta.f90
+++ rungekutta.f90
@@ -14,10 +14,6 @@ module CyclotronWithRungeKutta
     ! m: 質量
     double precision, parameter :: m = 1.0d0
 
-    ! B: 磁場
-    double precision, parameter :: B(3) = (/ 0, 0, 1 /)
-
-
     contains
 
         !!!*
@@ -25,6 +21,7 @@ module CyclotronWithRungeKutta
         !
         ! @subroutine run
         ! @public
+        ! @param {double} init_vel yの初速
         !!!
         subroutine run(init_vel)
             double precision init_vel
@@ -33,16 +30,17 @@ module CyclotronWithRungeKutta
             double precision :: t = 0
 
             ! r: 位置
-            double precision :: r(3) = (/ 0, 1, 0 /)
+            double precision :: r(3) = (/ 5.12d0, 0d0, 0d0 /) ! x = 磁場の配列の中央の値
 
             ! v: 速度
             double precision v(3)
+
             v(1) = init_vel
-            v(2) = 0
-            v(3) = 0
+            v(2) = init_vel
+            v(3) = 0d0
 
             do i = 1, 10000
-                write(*,*) r(1), r(2)
+                write(*,*) r(1), r(2), r(3)
                 v = v + rungekutta(v, t, acceleration)
                 r = r + v * h
             end do
@@ -50,15 +48,59 @@ module CyclotronWithRungeKutta
 
 
         !!!*
+        ! ある座標における磁場を取得
+        ! @function B
+        !!!
+        function B(r)
+
+            double precision B(3), r(3)
+            double precision x100, Bx
+            integer i, j, k
+            integer, dimension(1024) :: BArr = 1
+            !integer, dimension(1024) :: BArr
+
+            !do k = 1, 1024
+            !    BArr(k) = 1025 - k
+            !end do
+
+            x100 = r(1) * 100 + 1 ! 粒子のx座標の100倍 (BArrの単位に合わせるため. 1を足すことで配列に対応)
+            i    = int(x100) ! 粒子のいるBArrの左端
+            j    = i + 1     ! 粒子のいるBArrの右端
+
+            ! 粒子が磁場の右側の範囲外にいるとき
+            if (i > 1024) then
+                Bx = 0d0
+
+            ! 粒子が磁場の左側の範囲外にいるとき
+            elseif (i < 1) then
+                Bx = BArr(1)
+
+            ! 粒子のいるBArrの右端が未定義のときはそれを0とみなす
+            elseif (i == 1024) then
+                Bx = (j - x100) * BArr(i)
+
+            else
+                Bx = (j - x100) * BArr(i) + (x100 - i) * BArr(j)
+
+            endif
+
+            B(1) = Bx
+            B(2) = 0d0
+            B(3) = 0d0
+
+        end
+
+
+
+        !!!*
         ! 加速度
         ! @function acceleration
         !!!
         function acceleration(v, time, dr)
-            double precision acceleration(3)
+            double precision acceleration(3), v(3), dr(3)
             double precision time
-            double precision v(3)
-            double precision dr
-            acceleration = q / m * (E + cross(v, B(r + dr)))
+
+            acceleration = q / m * ( E + cross( v, B(r + dr) ) )
         end
 
 
@@ -82,54 +124,39 @@ module CyclotronWithRungeKutta
         function rungekutta(vec3, time, f)
 
             double precision rungekutta(3), vec3(3), k1(3), k2(3), k3(3), k4(3)
+            ! d1〜d2 微小時間で移動した距離
+            double precision d1(3), d2(3), d3(3), d4(3)
             double precision time
 
             ! 一次導関数微分方程式の右辺
-            ! 第３引数にはΔ vecΔ h (deltaVecTime) をいれる
+            ! 第３引数には 微小時間でのvec3の積分値 (台形近似) を与える
             interface
                 function f(vec3, time, deltaVecTime)
                     double precision f(3)
                     double precision vec3(3)
-                    double precision deltaVecTime(3)
                     double precision time
+                    double precision deltaVecTime(3)
+
                 end
             end interface
 
-            k1 = h * f(vec3, time, 0)
-            k2 = h * f(vec3 + k1 / 2, time + h / 2, k1 * h / 4)
-            k3 = h * f(vec3 + k2 / 2, time + h / 2, k2 * h / 4)
-            k4 = h * f(vec3 + k3, time + h, k3 * h)
+            d1 = (/0d0, 0d0, 0d0/)
+            k1 = f(vec3, time, d1)
 
-            rungekutta = (k1 + 2 * k2 + 2 * k3 + k4) / 6
+            d2 = (vec3 + vec3 + k1 * h / 2) * h / 2 / 2 ! 台形
+            k2 = f(vec3 + k1 * h / 2, time + h / 2, d2)
+
+            d3 = (vec3 + vec3 + k2 * h / 2) * h / 2 / 2 ! 台形
+            k3 = f(vec3 + k2 * h / 2, time + h / 2, d3)
+
+            d4 = (vec3 + vec3 + k3 * h) * h / 2 ! 台形
+            k4 = f(vec3 + k3 * h, time + h, d4)
+
+            rungekutta = h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
         end
     !end contains
 end
 
-module ReadFileModule
-    implicit none
-
-    contains
-
-        function readFile(filename, datanum)
-            double precision readFile(datanum), data(datanum)
-            character(len=20) filename
-            integer datanum, i
-            real value
-
-            open(17, file=filename, status='old')
-
-                do i = 1, datanum
-
-                    read(17, *)value
-                    data(i) = value
-
-                end do
-
-            close(17)
-            readFile = data
-        end
-
-end
 
 program main
     use CyclotronWithRungeKutta
